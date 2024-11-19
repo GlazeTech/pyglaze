789d6106d9aa06bd4bd72c8bb3c3fe475f86ee67
Bjørn Mølvig <34375265+Duckling92@users.noreply.github.com>
2024-11-19 13:31:22 +0100

    bugfix to polarity switch of pulse. (#14)

--- src/pyglaze/scanning/scanner.py
+++ src/pyglaze/scanning/scanner.py
@@ -258,10 +258,13 @@ def _scanner_factory(config: DeviceConfiguration) -> _ScannerImplementation:
 
 class _LockinPhaseEstimator:
     def __init__(
-        self: _LockinPhaseEstimator, r_threshold_for_update: float = 2.0
+        self: _LockinPhaseEstimator,
+        r_threshold_for_update: float = 2.0,
+        theta_threshold_for_adjustment: float = 1.0,
     ) -> None:
-        self.phase_estimate: float | None = None
         self.r_threshold_for_update = r_threshold_for_update
+        self.theta_threshold_for_adjustment = theta_threshold_for_adjustment
+        self.phase_estimate: float | None = None
         self._radius_of_est: float | None = None
 
     def update_estimate(
@@ -274,7 +277,11 @@ class _LockinPhaseEstimator:
             self._set_estimates(theta_at_max, r_max)
             return
 
-        if r_max > self.r_threshold_for_update * self._radius_of_est:
+        if r_max > self.r_threshold_for_update * self._radius_of_est or (
+            r_max > self._radius_of_est
+            and abs(theta_at_max - self.phase_estimate)
+            < self.theta_threshold_for_adjustment
+        ):
             self._set_estimates(theta_at_max, r_max)
 
     def _set_estimates(
