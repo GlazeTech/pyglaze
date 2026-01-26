6b23e7fd79dca503e49447f6af748379b7fd7522
Bjørn Mølvig <bjoernmoelvig@gmail.com>
2026-01-26 15:56:14 +0100

    Fix wheel check and install to comply with new UV in CI

--- scripts/install_wheel.py
+++ scripts/install_wheel.py
@@ -1,11 +1,10 @@
 # install_wheel.py
 import subprocess
-import sys
 from pathlib import Path
 
 wheels = list(Path.cwd().glob("dist/*.whl"))
 if wheels:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", wheels[0]])  # noqa: S603
+    subprocess.check_call(["uv", "pip", "install", str(wheels[0])])  # noqa: S603, S607
 else:
     msg = "No .whl files found in the dist directory."
     raise FileNotFoundError(msg)
